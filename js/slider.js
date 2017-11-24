ec1e6e6c71873a3df656ebab0f21385715a7dc28
Serge <vspox@yandex.ru>
2017-11-24 14:15:54 +0200

    initial commit

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+
+$('.carousel').carousel({
+    interval:2000,
+    arrows: false,
+    keyboard: true
+})
+             //modal window
+$('#exampleModal').modal({
+    keyboard: true,
+    backdrop: static
+})
